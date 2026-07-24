e1ecdae8b9a6669ea9cefa3b8b4ada9b3b5df1f5
Veeramani-07 <veeramani.k2024ece@sece.ac.in>
2026-07-24 23:56:44 +0530

    Fix Spring JPA repository component scan

--- agent-lock-backend/src/main/java/com/agentlock/agent_lock_backend/AgentLockBackendApplication.java
+++ agent-lock-backend/src/main/java/com/agentlock/agent_lock_backend/AgentLockBackendApplication.java
@@ -1,14 +1,13 @@
 package com.agentlock.agent_lock_backend;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = {"Repository", "repository", "com.agentlock"}) // Spring-a explicit-a scan panna vaikkum
-@EntityScan(basePackages = {"Model", "model", "entity", "com.agentlock"})
+@EnableJpaRepositories(basePackages = {"Repository", "repository", "com.agentlock"})
+@EntityScan(basePackages = {"Entity", "entity", "Model", "model", "com.agentlock"}) // "Entity" explicit-a add panni erukken!
 public class AgentLockBackendApplication {
 
     public static void main(String[] args) {
